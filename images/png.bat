ed9fb1f320aea4b419b3149d39473377086d11b0
Christoph Kieslich <c.kieslich@gmail.com>
2014-05-09 14:08:14 +0200

    test

--- images/png.bat
+++ images/png.bat
@@ -1,3 +1,3 @@
-mogrify -density 150x150 -resize 600 -format png *.pdf
-mogrify -quality 100 -format jpg -fill "#FFFFFF" -opaque none *.png
+mogrify -density 300x300 -resize 600 -format png *.pdf
+REM mogrify -quality 100 -format jpg -fill "#FFFFFF" -opaque none *.png
 optipng *.png
